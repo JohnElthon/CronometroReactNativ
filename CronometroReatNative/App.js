0e3f1ad01bc0962c23d74a8810efe61138e891b4
JohnElthon <johnelthon1@gmail.com>
2025-04-08 11:51:05 -0300

    .

--- CronometroReatNative/App.js
+++ CronometroReatNative/App.js
@@ -1,20 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React , {Component} from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+ 
+class App extends Component {
+  constructor(props){
+  super (props);
+  this.state={
+    numero:0,
+ 
+  };
+ 
+this.vai = this.vai.bind(this);
+this.limpar = this.limpar.bind(this);
+ 
+this.timer = null;
+ 
+  }
+  vai(){
+   if(this.timer != null){
+ 
+   }
+ 
+   else {
+    this.timer = setInterval(()=>(
+      this.setState({numero: this.setState.numero +0.1 })
+    ),100);
+   
+  }
+ 
+  }
+  limpar(){
+    if (this.timer != null){
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+    this.setState({
+      numero: 0,
+    })
+   
+  }
+  
+    
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ 
+  render(){
+    return(
+      <View style={styles.container}>
+        <Image source={require('./src/cronometro.png')}></Image>
+        <Text style= {styles.timer} > {this.state.numero.toFixed(1)}</Text>
+        <View style = {styles.btnArea}>
+          <TouchableOpacity style={styles.btn} >
+            <Text style = {styles.btnTexto} onPress= {this.vai}> Vai</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style= {styles.btn}>
+            <Text style= {styles.btnTexto} onpress = {this.limpar}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems:'center',
+    justifyContent: 'center',
+    backgroundColor: '#00aeef'
+  },
+  timer:{
+    color:'white',
+    fontSize: 65,
+    fontWeight: 'bold',
+    marginTop: -150,
+ 
+  },
+  btnArea:{
+    marginTop: 70,
+    height: 40,
+    flexDirection:'row',
+  },
+  btn:{
+    flex:1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ffff',
+    height: 40,
+    margin: 20,
+    borderRadius: 10,
+ 
   },
+ 
+  btnTexto:{
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#00aeef'
+  },
+ 
 });
+ 
+export default App;
